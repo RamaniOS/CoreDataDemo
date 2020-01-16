6a3a84b34f2a02d9766a93661bf9f15597df4040
Ramanpreet Singh <ramanpreet230@gmail.com>
2020-01-16 15:03:01 -0500

    Initial Commit

---
+++ CoreDataDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by Ramanpreet Singh on 2020-01-16.
+//  Copyright © 2020 Ramanpreet Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
